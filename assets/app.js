ee56013b0bbacf6f4ecb4b795840876fe2a2150c
Marcel <martinez.otano2000@web.de>
2023-01-21 01:31:49 +0100

    Add darkModeToggle

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,50 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+/**
+ * Darkmode toggle
+ */
+
+//icons
+// const sunIcon = document.querySelector('.sun');
+// const moonIcon = document.querySelector('.moon');
+const button = document.querySelector('.theme-button');
+
+//Icon/Text Toggling
+// const iconToggle = () => {
+//     moonIcon.classList.toggle('display-none');
+//     sunIcon.classList.toggle('display-none');
+// }
+
+//Initial Theme Check
+const themeCheck = () => {
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark');
+        // moonIcon.classList.add('.display-none');
+        return;
+    }
+    // sunIcon.classList.add('.display-none');
+}
+
+//Manual Theme Switch
+const themeSwitch = () => {
+    if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+        // iconToggle();
+        return;
+    }
+    document.documentElement.classList.add('dark')
+    localStorage.setItem('theme', 'dark');
+    // iconToggle();
+}
+
+//call theme switch on clicking buttons
+button.addEventListener('click', () => {
+    themeSwitch();
+});
+
+//invoke theme check on initial load
+
+themeCheck();
