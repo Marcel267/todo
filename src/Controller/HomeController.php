d818fbe00fffe8bd90236eb5e8b55e8a09a7561d
Marcel <martinez.otano2000@web.de>
2023-01-28 20:55:23 +0100

    Add Entities Note, Category

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Note;
+use App\Repository\NoteRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $notes = $doctrine->getRepository(Note::class)->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'notes' => $notes
         ]);
+    }
+
+    #[Route('/create', name: 'app_create')]
+    public function create(NoteRepository $noteRepository): JsonResponse
+    {
+
+        $note = new Note();
+        $note->setContent('Test Notiz');
+
+        $noteRepository->save($note, true);
+
+
+        return new JsonResponse('Note Created');
     }
 }
---
+++ src/Entity/Note.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NoteRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NoteRepository::class)]
+class Note
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $content = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $created = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $dueDate = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $reminderDate = null;
+
+    public function __construct()
+    {
+        $this->created = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreated(): ?\DateTimeInterface
+    {
+        return $this->created;
+    }
+
+    public function setCreated(\DateTimeInterface $created): self
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    public function getDueDate(): ?\DateTimeInterface
+    {
+        return $this->dueDate;
+    }
+
+    public function setDueDate(?\DateTimeInterface $dueDate): self
+    {
+        $this->dueDate = $dueDate;
+
+        return $this;
+    }
+
+    public function getReminderDate(): ?\DateTimeInterface
+    {
+        return $this->reminderDate;
+    }
+
+    public function setReminderDate(?\DateTimeInterface $reminderDate): self
+    {
+        $this->reminderDate = $reminderDate;
+
+        return $this;
+    }
+}
