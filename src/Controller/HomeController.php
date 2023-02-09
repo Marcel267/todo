2ffdd4b3ec4fc3188ba5a4d8dec62080c0b131dd
Marcel <martinez.otano2000@web.de>
2023-02-09 17:40:58 +0100

    Make sidebar active tab work

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,18 +12,66 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(ManagerRegistry $doctrine): Response
+    #[Route('/', name: 'redirect')]
+    public function test()
     {
-        $notes = $doctrine->getRepository(Note::class)->findAll();
+        return $this->redirect($this->generateUrl('home', ['category' => 'all']));
+    }
+
+    #[Route('/{category}', name: 'home')]
+    public function index(ManagerRegistry $doctrine, $category): Response
+    {
+        $notes = null;
+
+        //check if all or marked category is called
+        if ($category === null || $category === 'all') {
+            $notes = $doctrine->getRepository(Note::class)->findAll();
+            $icon = 'home';
+            $title = 'Alle Notizen';
+        } elseif ($category === 'marked') {
+            $notes = $doctrine->getRepository(Note::class)->findAll();
+            $icon = 'star';
+            $title = 'Markierte Notizen';
+        } else {
+            //@ TODO: get all custom notes
+            $notes = $doctrine->getRepository(Note::class)->findAll();
+            $icon = 'list';
+            $title = 'Eigene Liste';
+        }
+
+        // $notes = $doctrine->getRepository(Note::class)->findAll();
+        // $icon = 'home';
+        // $title = 'Alle Notizen';
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'notes' => $notes
+            'notes' => $notes,
+            'icon' => $icon,
+            'title' => $title,
+            'category' => $category
         ]);
     }
 
-    #[Route('/create', name: 'app_create')]
+
+    // #[Route('/all', name: 'all')]
+    // public function all(ManagerRegistry $doctrine, $category): Response
+    // {
+    //     $notes = $doctrine->getRepository(Note::class)->findAll();
+    //     $icon = 'home';
+    //     $title = 'Alle Notizen';
+
+    //     return $this->render('home/index.html.twig', [
+    //         'controller_name' => 'HomeController',
+    //         'notes' => $notes,
+    //         'icon' => $icon,
+    //         'title' => $title,
+    //         'category' => $category
+    //     ]);
+    // }
+
+
+
+    #[Route('/create', name: 'create')]
     public function create(NoteRepository $noteRepository): JsonResponse
     {
 
